e0923719f026d06f66f5fc2b2fa7fc364bf3d9d0
lonesski <lonesski@gmail.com>
2021-05-19 13:34:27 +0300

    add hooks

--- src/channels/channel.ts
+++ src/channels/channel.ts
@@ -1,3 +1,5 @@
+let fs = require('fs');
+let request = require('request');
 import { PresenceChannel } from './presence-channel';
 import { PrivateChannel } from './private-channel';
 import { Log } from './../log';
@@ -24,11 +26,19 @@ export class Channel {
     presence: PresenceChannel;
 
     /**
+     * Request client.
+     *
+     * @type {any}
+     */
+    private request: any;
+
+    /**
      * Create a new channel instance.
      */
     constructor(private io, private options) {
         this.private = new PrivateChannel(options);
         this.presence = new PresenceChannel(io, options);
+        this.request = request;
 
         if (this.options.devMode) {
             Log.success('Channels are ready.');
@@ -66,8 +76,78 @@ export class Channel {
                 this.io.sockets.connected[socket.id]
                     .broadcast.to(data.channel)
                     .emit(data.event, data.channel, data.data);
+
+                if (data.event!='client-typing') { // block hooks from firing on client typing
+                    data.data.event=data.event; // added client event name to catch it on server side
+                    this.hook(socket, data.channel, data.auth, "client_event", data.data);
+                }
             }
         }
+    }
+
+    /**
+     *
+     * @param {any} socket
+     * @param {string} channel
+     * @param {object} auth
+     * @param {string} event
+     * @param {object} payload
+     */
+    hook(socket:any, channel: any, auth: any, event: string, payload: object) {
+        if (typeof this.options.hookEndpoint == 'undefined' ||
+            !this.options.hookEndpoint) {
+            return;
+        }
+
+        let hookEndpoint = this.options.hookEndpoint;
+
+        let options = this.prepareHookHeaders(socket, auth, channel, hookEndpoint, event, payload);
+
+        this.request.post(options, (error, response, body, next) => {
+            if (error) {
+                if (this.options.devMode) {
+                    Log.error(`[${new Date().toLocaleTimeString()}] - Error call ${event} hook ${socket.id} for ${options.form.channel}`);
+                }
+                Log.error(error);
+            } else if (response.statusCode !== 200) {
+                if (this.options.devMode) {
+                    Log.warning(`[${new Date().toLocaleTimeString()}] - Error call ${event} hook ${socket.id} for ${options.form.channel}`);
+                    Log.error(response.body);
+                }
+            } else {
+                if (this.options.devMode) {
+                    Log.info(`[${new Date().toLocaleTimeString()}] - Call ${event} hook for ${socket.id} for ${options.form.channel}: ${response.body}`);
+                }
+            }
+        });
+    }
+
+    /**
+     * Prepare headers for request to app server.
+     *
+     * @param {any} socket
+     * @param {any} auth
+     * @param {string} channel
+     * @param {string} hookEndpoint
+     * @param {string} event
+     * @param {any} payload
+     * @returns {any}
+     */
+    prepareHookHeaders(socket: any, auth: any, channel: string, hookEndpoint: string, event: string, payload: any): any {
+        let hookHost = this.options.hookHost ? this.options.hookHost : this.options.authHost;
+        let options = {
+            url: hookHost + hookEndpoint,
+            form: {
+                event: event,
+                channel: channel,
+                payload: payload
+            },
+            headers: (auth && auth.headers) ? auth.headers : {}
+        };
+
+        options.headers['Cookie'] = socket.request.headers.cookie;
+        options.headers['X-Requested-With'] = 'XMLHttpRequest';
+        return options;
     }
 
     /**
